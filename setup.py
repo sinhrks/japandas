2513a1bc4a08974b197d958f097cbf8fd8ecfb60
sinhrks <sinhrks@gmail.com>
2015-02-12 23:31:05 +0900

    REL: v0.0.2 preparation

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ REQUIREMENTS = 'requirements.txt'
 # required modules for test and doc build
 REQUIREMENTS_TEST = 'requirements_test.txt'
 
-VERSION = '0.0.1'
+VERSION = '0.0.2dev'
 
 def read(fname):
   # file must be read as utf-8 in py3 to avoid to be bytes
