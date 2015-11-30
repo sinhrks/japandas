6eed62a01a6e57b821b035ba8c9d5e6fc289374b
Sinhrks <sinhrks@gmail.com>
2015-11-30 18:53:41 +0900

    REL: Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PACKAGE = 'japandas'
 README = 'README.rst'
 REQUIREMENTS = 'requirements.txt'
 
-VERSION = '0.2.0'
+VERSION = '0.3.0.dev'
 
 def read(fname):
   # file must be read as utf-8 in py3 to avoid to be bytes
