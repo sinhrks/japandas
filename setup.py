0a19c03bdad8b1c2236be610ba76055996fa71bb
sinhrks <sinhrks@gmail.com>
2016-01-03 11:25:04 +0900

    REL: v0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PACKAGE = 'japandas'
 README = 'README.rst'
 REQUIREMENTS = 'requirements.txt'
 
-VERSION = '0.3.0.dev'
+VERSION = '0.2.1'
 
 def read(fname):
   # file must be read as utf-8 in py3 to avoid to be bytes
